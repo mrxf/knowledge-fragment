7180facaf892576dadad7153fad7fa1d60a9efc3
ZHANGSHUYUAN <zhangshuyuan@kingsoft.com>
2021-05-21 17:58:24 +0800

    chore: 修改域名

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: '知识碎片',
   tagline: '记录技术学习内容',
-  url: 'study.thisjs.com',
+  url: 'knowledge-fragment.vercel.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
